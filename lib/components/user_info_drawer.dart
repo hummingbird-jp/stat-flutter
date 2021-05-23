5b8ac5f7eae99678eeacd7e60cddb28e50cb2452
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-05-23 15:25:35 +0900

    refactor(user_info_drawer): extract UserInfoDrawer widget from team_screen

---
+++ lib/components/user_info_drawer.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:stat_flutter/screens/welcome_screen.dart';
+
+class UserInfoDrawer extends StatelessWidget {
+  const UserInfoDrawer({
+    Key key,
+    @required this.loggedInUser,
+    @required FirebaseAuth auth,
+  })  : _auth = auth,
+        super(key: key);
+
+  final User loggedInUser;
+  final FirebaseAuth _auth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountEmail: Text('${loggedInUser.email}'),
+            accountName: Text('Test User'),
+          ),
+          ListTile(
+            title: Text(
+              'Log out',
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+              ),
+            ),
+            onTap: () async {
+              await _auth.signOut();
+              Navigator.pushNamed(context, WelcomeScreen.id);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
