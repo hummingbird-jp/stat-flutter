774634ad784d4b8c975fbc4defa77ffe6fa5adf4
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-05-23 14:00:08 +0900

    feat(getstatus): sendStatus() now sends random status: Fine/Not Fine

--- lib/screens/team_screen.dart
+++ lib/screens/team_screen.dart
@@ -1,17 +1,19 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:stat_flutter/components/person_with_status.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:stat_flutter/components/rounded_button.dart';
 import 'package:stat_flutter/components/rounded_outline_button.dart';
-import 'package:stat_flutter/screens/login_screen.dart';
+import 'package:stat_flutter/screens/welcome_screen.dart';
 
-//enum Status {
-//  close,
-//  check,
-//}
+enum Status {
+  fine,
+  notFine,
+}
 
 class TeamScreen extends StatefulWidget {
   static const String id = 'team_screen';
@@ -26,6 +28,7 @@ class _TeamScreenState extends State<TeamScreen> {
   User loggedInUser;
   String status;
   bool isUpdateStopped = false;
+  bool isInProgress = false;
 
   @override
   void initState() {
@@ -47,25 +50,40 @@ class _TeamScreenState extends State<TeamScreen> {
     }
   }
 
-  void statusStream() async {
-    await for (var snapshot in _firestore.collection('statuses').snapshots()) {
-      for (var status in snapshot.docs) {
-        print(status.data());
-      }
-    }
-  }
+  // TODO: implement statusStream
+  //void statusStream() async {
+  //  await for (var snapshot in _firestore.collection('statuses').snapshots()) {
+  //    for (var status in snapshot.docs) {
+  //      print(status.data());
+  //    }
+  //  }
+  //}
 
-  void sendStatus(String status) {
+  void sendStatus() {
     const updateInterval = Duration(seconds: 5);
 
-    Timer.periodic(updateInterval, (timer) {
+    Timer.periodic(updateInterval, (timer) async {
       if (isUpdateStopped == false) {
-        _firestore.collection('statuses').add({
-          'status': status,
-          'userEmail': loggedInUser.email,
-        });
+        try {
+          Status status = getStatus();
+
+          await _firestore.collection('statuses').add({
+            'status': status == Status.fine ? 'Fine' : 'Not Fine',
+            'userEmail': loggedInUser.email,
+          });
+        } on FirebaseException catch (e) {
+          print('FirebaseException: $e');
+        } catch (e) {
+          print('Other Exception: $e');
+        }
       }
     });
+  }
+
+  Status getStatus() {
+    int pick = new Random().nextInt(Status.values.length);
+    print(Status.values[pick]);
+    return Status.values[pick];
   }
 
   @override
@@ -74,57 +92,59 @@ class _TeamScreenState extends State<TeamScreen> {
       appBar: AppBar(
         title: Text('Stat! Flutter'),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 100.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: PersonWithStatus(
-                    userImage: NetworkImage(
-                        'https://media-exp1.licdn.com/dms/image/C5603AQG7D1xY_YtA6g/profile-displayphoto-shrink_400_400/0/1615853413089?e=1626912000&v=beta&t=aWaLslh_IrP6C-OHWnEYOM_4v5tfv35TgWfM5foBKcs'),
-                    status: Icons.close,
+      body: ModalProgressHUD(
+        inAsyncCall: isInProgress,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 100.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: PersonWithStatus(
+                      userImage: NetworkImage(
+                          'https://media-exp1.licdn.com/dms/image/C5603AQG7D1xY_YtA6g/profile-displayphoto-shrink_400_400/0/1615853413089?e=1626912000&v=beta&t=aWaLslh_IrP6C-OHWnEYOM_4v5tfv35TgWfM5foBKcs'),
+                      status: Icons.close,
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: PersonWithStatus(
-                    userImage: NetworkImage(
-                        'https://media-exp1.licdn.com/dms/image/C5603AQGGhBt6Ckxytg/profile-displayphoto-shrink_400_400/0/1604068708768?e=1626912000&v=beta&t=vc4GysO1ftX8OUB6psN6XJkuz-QdNfygRx2WrCrPgcA'),
-                    status: Icons.check,
+                  Expanded(
+                    child: PersonWithStatus(
+                      userImage: NetworkImage(
+                          'https://media-exp1.licdn.com/dms/image/C5603AQGGhBt6Ckxytg/profile-displayphoto-shrink_400_400/0/1604068708768?e=1626912000&v=beta&t=vc4GysO1ftX8OUB6psN6XJkuz-QdNfygRx2WrCrPgcA'),
+                      status: Icons.check,
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: PersonWithStatus(
-                    userImage: NetworkImage(
-                        'https://media-exp1.licdn.com/dms/image/C5103AQEQRhS5GU68WA/profile-displayphoto-shrink_400_400/0/1545331992316?e=1626912000&v=beta&t=cJdEYKOYACtwZ0-qRA3S3b_Ddax4NH6Z-1dGGCNmtrg'),
-                    status: Icons.close,
+                  Expanded(
+                    child: PersonWithStatus(
+                      userImage: NetworkImage(
+                          'https://media-exp1.licdn.com/dms/image/C5103AQEQRhS5GU68WA/profile-displayphoto-shrink_400_400/0/1545331992316?e=1626912000&v=beta&t=cJdEYKOYACtwZ0-qRA3S3b_Ddax4NH6Z-1dGGCNmtrg'),
+                      status: Icons.close,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            RoundedButton(
-              color: Theme.of(context).colorScheme.secondary,
-              label: 'RUN!',
-              onPressed: () {
-                isUpdateStopped = false;
-                String status = 'fine';
-                sendStatus(status);
-              },
-            ),
-            RoundedOutlineButton(
-              color: Theme.of(context).colorScheme.secondary,
-              label: 'STOP',
-              onPressed: () {
-                isUpdateStopped = true;
-              },
-            )
-          ],
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              RoundedButton(
+                color: Theme.of(context).colorScheme.secondary,
+                label: 'RUN!',
+                onPressed: () async {
+                  isUpdateStopped = false;
+                  sendStatus(); // repeatitive
+                },
+              ),
+              RoundedOutlineButton(
+                color: Theme.of(context).colorScheme.secondary,
+                label: 'STOP',
+                onPressed: () {
+                  isUpdateStopped = true;
+                },
+              )
+            ],
+          ),
         ),
       ),
       drawer: Drawer(
@@ -143,9 +163,13 @@ class _TeamScreenState extends State<TeamScreen> {
                 ),
               ),
               onTap: () async {
+                setState(() {
+                  isInProgress = true;
+                });
+
                 await _auth.signOut();
-                Navigator.pop(context);
-                Navigator.pushNamed(context, LoginScreen.id);
+                isInProgress = false;
+                Navigator.pushNamed(context, WelcomeScreen.id);
               },
             ),
           ],
