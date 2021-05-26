c704739717f583a646b3792738f910b64c1ef8e1
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-05-27 00:11:07 +0900

    refactor(modal_progress_hud): remove this
    because this package doesn't support null-safety

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:stat_flutter/components/rounded_outline_button.dart';
 import 'package:stat_flutter/constants.dart';
 import 'package:stat_flutter/screens/login_failed_screen.dart';
@@ -15,85 +14,76 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
-  String email;
-  String password;
-  bool isInProgress = false;
+  late String email;
+  late String password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ModalProgressHUD(
-        inAsyncCall: isInProgress,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 200.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Hero(
-                tag: 'Stat! logo',
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('images/logo.jpg'),
-                  radius: 50.0,
-                ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 200.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Hero(
+              tag: 'Stat! logo',
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/logo.jpg'),
+                radius: 50.0,
               ),
-              SizedBox(
-                height: 30.0,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                email = value;
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                hintStyle: kHintTextStyle,
+                hintText: 'Enter your email',
               ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  hintStyle: kHintTextStyle,
-                  hintText: 'Enter your email',
-                ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                password = value;
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                hintStyle: kHintTextStyle,
+                hintText: 'Enter your password',
               ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  hintStyle: kHintTextStyle,
-                  hintText: 'Enter your password',
-                ),
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              Hero(
-                tag: 'login button',
-                child: RoundedOutlineButton(
-                  color: Theme.of(context).colorScheme.secondary,
-                  label: 'Log In',
-                  onPressed: () async {
-                    setState(() {
-                      isInProgress = true;
-                    });
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            Hero(
+              tag: 'login button',
+              child: RoundedOutlineButton(
+                color: Theme.of(context).colorScheme.secondary,
+                label: 'Log In',
+                onPressed: () async {
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
 
-                    try {
-                      final user = await _auth.signInWithEmailAndPassword(
-                          email: email, password: password);
-
-                      if (user != null) {
-                        Navigator.pushNamed(context, TeamScreen.id);
-                      }
-                      isInProgress = false;
-                    } catch (e) {
-                      Navigator.pushNamed(context, LoginFailedScreen.id);
+                    if (user != null) {
+                      Navigator.pushNamed(context, TeamScreen.id);
                     }
-                  },
-                ),
+                  } catch (e) {
+                    Navigator.pushNamed(context, LoginFailedScreen.id);
+                  }
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:stat_flutter/constants.dart';
 import 'package:stat_flutter/screens/team_screen.dart';
 import 'package:stat_flutter/components/rounded_button.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -14,102 +13,93 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
-  String email;
-  String password;
-  String displayName;
-  bool isInProgress = false;
+  late String email;
+  late String password;
+  String? displayName;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ModalProgressHUD(
-        inAsyncCall: isInProgress,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 200.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Hero(
-                tag: 'Stat! logo',
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('images/logo.jpg'),
-                  radius: 50.0,
-                ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 200.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Hero(
+              tag: 'Stat! logo',
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/logo.jpg'),
+                radius: 50.0,
               ),
-              SizedBox(
-                height: 30.0,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                email = value;
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                hintStyle: kHintTextStyle,
+                hintText: 'What is your email?',
               ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  hintStyle: kHintTextStyle,
-                  hintText: 'What is your email?',
-                ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                password = value;
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                hintStyle: kHintTextStyle,
+                hintText: 'What is your new password?',
               ),
-              SizedBox(
-                height: 8.0,
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                displayName = value;
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                hintStyle: kHintTextStyle,
+                hintText: 'What do people call you?',
               ),
-              TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  hintStyle: kHintTextStyle,
-                  hintText: 'What is your new password?',
-                ),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  displayName = value;
-                },
-                decoration: kTextFieldDecoration.copyWith(
-                  hintStyle: kHintTextStyle,
-                  hintText: 'What do people call you?',
-                ),
-              ),
-              SizedBox(
-                height: 24.0,
-              ),
-              Hero(
-                tag: 'register button',
-                child: RoundedButton(
-                  color: Theme.of(context).colorScheme.secondary,
-                  label: 'Register',
-                  onPressed: () async {
-                    setState(() {
-                      isInProgress = true;
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            Hero(
+              tag: 'register button',
+              child: RoundedButton(
+                color: Theme.of(context).colorScheme.secondary,
+                label: 'Register',
+                onPressed: () async {
+                  try {
+                    await _auth
+                        .createUserWithEmailAndPassword(
+                            email: email, password: password)
+                        .then((result) {
+                      return result.user!.updateProfile(
+                        displayName: displayName,
+                      );
                     });
-
-                    try {
-                      await _auth
-                          .createUserWithEmailAndPassword(
-                              email: email, password: password)
-                          .then((result) {
-                        return result.user.updateProfile(
-                          displayName: displayName,
-                        );
-                      });
-                      isInProgress = false;
-                      Navigator.pushNamed(context, TeamScreen.id);
-                    } catch (e) {
-                      print(e);
-                    }
-                  },
-                ),
+                    Navigator.pushNamed(context, TeamScreen.id);
+                  } catch (e) {
+                    print(e);
+                  }
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
